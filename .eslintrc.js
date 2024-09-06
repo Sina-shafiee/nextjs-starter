237c16240ff5bd55a6d01acfea76a8c795805dde
Sina Shafiee <alisinashafieezada@gmail.com>
2024-09-06 04:22:00 +0330

    chore: create-next-app init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+const { init } = require('@fullstacksjs/eslint-config/init');
+
+module.exports = init({
+  root: true,
+  modules: {
+    auto: true,
+    esm: true,
+    next: true,
+    typescript: {
+      parserProject: ['./tsconfig.eslint.json'],
+      resolverProject: ['./tsconfig.json'],
+    },
+  },
+});
